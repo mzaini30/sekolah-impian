2f8b1dd3692c68e2c5382a1a7cd52e9092617ae7
mzaini30 <muhzaini30@gmail.com>
2018-05-13 12:47:11 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,5 +1,7 @@
 $('.sidenav').sidenav()
-$('.medium h1, .medium p').addClass('animated')
+$(window).scroll(function(){
+	$(this).is('visible').addClass('animated')
+})
 list_animate = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'fadeIn', 'fadeInDown', 'fadeInDownBig', 'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'flipInX', 'flipInY', 'lightSpeedIn', 'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight', 'hinge', 'jackInTheBox', 'rollIn', 'zoomIn', 'zoomInDown', 'zoomInLeft', 'zoomInRight', 'zoomInUp', 'slideInDown', 'slideInLeft', 'slideInRight', 'slideInUp']
 $('.medium h1, .medium p').each(function(){
 	$(this).addClass(list_animate[~~(Math.random() * list_animate.length)])
