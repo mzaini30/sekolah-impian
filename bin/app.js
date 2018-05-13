99e2538b67d520ae4a6b3123873b32decaad2ac9
mzaini30 <muhzaini30@gmail.com>
2018-05-13 12:50:19 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,14 @@
 $('.sidenav').sidenav()
+jika_elemen_nampak = function(){
+	$('.medium h1, .medium p').each(function(){
+		if($(this).is(':visible')){
+			$(this).addClass('animated')
+		}
+	})
+}
+jika_elemen_nampak()
 $(window).scroll(function(){
-	$(this).is('visible').addClass('animated')
+	jika_elemen_nampak()
 })
 list_animate = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'fadeIn', 'fadeInDown', 'fadeInDownBig', 'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'flipInX', 'flipInY', 'lightSpeedIn', 'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight', 'hinge', 'jackInTheBox', 'rollIn', 'zoomIn', 'zoomInDown', 'zoomInLeft', 'zoomInRight', 'zoomInUp', 'slideInDown', 'slideInLeft', 'slideInRight', 'slideInUp']
 $('.medium h1, .medium p').each(function(){
