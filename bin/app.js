f4fd33a36ab428cdd9a3c826cbb7cd78a57cf007
mzaini30 <muhzaini30@gmail.com>
2018-05-13 13:32:53 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,9 +1,21 @@
 $(function(){
 	$('.sidenav').sidenav()
 	elemen = $('.medium h1, .medium p')
-	elemen.addClass('animated')
 	list_animate = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'fadeIn', 'fadeInDown', 'fadeInDownBig', 'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'flipInX', 'flipInY', 'lightSpeedIn', 'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight', 'jackInTheBox', 'rollIn', 'zoomIn', 'zoomInDown', 'zoomInLeft', 'zoomInRight', 'zoomInUp', 'slideInDown', 'slideInLeft', 'slideInRight', 'slideInUp']
 	elemen.each(function(){
-		$(this).addClass(list_animate[~~(Math.random() * list_animate.length)])
+		$(this).addClass("hidden").viewportChecker({
+			classToAdd: function(){
+				return 'visible animated ' + list_animate[~~(Math.random() * list_animate.length)]
+			},
+			offset: 100
+		});
+		// $(this).addClass('visible animated').addClass(list_animate[~~(Math.random() * list_animate.length)])
 	})
 })
+
+// $(document).ready(function() {
+//     elemen.addClass("hidden").viewportChecker({
+// 		classToAdd: 'visible animated fadeIn',
+// 		offset: 100
+// 	});
+// });
