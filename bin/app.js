f6c6c9e309ee954aec3ffeda59c7c384da4c8941
mzaini30 <muhzaini30@gmail.com>
2018-05-13 13:57:11 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,6 @@
 $(function(){
 	$('.sidenav').sidenav()
-	elemen = $('.medium h1, .medium p, .medium blockquote')
+	elemen = $('.medium h1, .medium p, .medium blockquote, .btn')
 	list_animate = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'fadeIn', 'fadeInDown', 'fadeInDownBig', 'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'flipInX', 'flipInY', 'lightSpeedIn', 'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight', 'jackInTheBox', 'rollIn', 'zoomIn', 'zoomInDown', 'zoomInLeft', 'zoomInRight', 'zoomInUp', 'slideInDown', 'slideInLeft', 'slideInRight', 'slideInUp']
 	elemen.each(function(){
 		$(this).addClass("hidden").viewportChecker({
