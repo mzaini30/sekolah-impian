56ef23ea7f7a0568bb0a35ab1a33c97f788a18ca
mzaini30 <muhzaini30@gmail.com>
2018-05-13 13:58:48 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,13 +1,12 @@
 $(function(){
 	$('.sidenav').sidenav()
-	elemen = $('.medium h1, .medium p, .medium blockquote, .btn')
+	elemen = $('.medium h1, .medium p, .medium blockquote')
 	list_animate = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'fadeIn', 'fadeInDown', 'fadeInDownBig', 'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'flipInX', 'flipInY', 'lightSpeedIn', 'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight', 'jackInTheBox', 'rollIn', 'zoomIn', 'zoomInDown', 'zoomInLeft', 'zoomInRight', 'zoomInUp', 'slideInDown', 'slideInLeft', 'slideInRight', 'slideInUp']
 	elemen.each(function(){
 		$(this).addClass("hidden").viewportChecker({
 			classToAdd: function(){
 				return 'visible animated ' + list_animate[~~(Math.random() * list_animate.length)]
-			},
-			offset: 100
+			}
 		});
 	})
 })
